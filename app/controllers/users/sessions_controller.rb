2cc507fca0ad3844070c85e0e9d03048ace98418
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-10-16 22:45:04 +0000

    Create user controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  devise_for :users, controllers: { registrations: "users/registrations" }
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
