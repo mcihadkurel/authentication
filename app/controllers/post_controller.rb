d49d29c72e31d80cf48f47dd960b35eb1d97acf4
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-10-19 22:42:51 +0300

    Add method actions

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,10 +1,24 @@
 class PostController < ApplicationController
 def new
-
+    @post = current_user.posts.new
 end
 
 def create
+    @post = current_user.posts.new(post_params)
 
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
 end
+
+def index
+    @posts = Post.all
+  end
 
 end
