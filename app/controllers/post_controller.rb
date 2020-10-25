5f08fb41b665434a46682c98407ece3bcf3b0316
Muhammed Cihad Kurel <65771498+mcihadkurel@users.noreply.github.com>
2020-10-25 22:15:48 +0300

    Remove respond action

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -5,15 +5,6 @@ end
 
 def create
     @post = current_user.posts.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
     end
 end
 
